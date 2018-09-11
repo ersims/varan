9aefcb72835bcf5943115336f24ba420428c3f71
ersims <ersims88@gmail.com>
2018-09-11 21:20:19 +0200

    fix: version bump template package.json

--- scripts/bumpTemplateVersion.js
+++ scripts/bumpTemplateVersion.js
@@ -19,13 +19,7 @@ try {
   fs.writeFileSync(pkgFile, JSON.stringify(templatePkg, null, 2), 'UTF-8');
 
   // Add changed file to git
-  const procGit = execa.sync('git', ['add', pkgFile], {
-    stdio: 'inherit',
-  });
-  if (procGit.status !== 0) {
-    console.error(`Failed to git add template/package.json`);
-    process.exit(1);
-  }
+  execa.sync('git', ['add', pkgFile]);
 } catch (err) {
   console.error(`Failed to update template dependency version`);
   process.exit(1);
